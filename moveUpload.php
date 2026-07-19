ec145503e705e6492813bed3abbf71d541b60322
ByKimhy <kimhyby99@gmail.com>
2026-07-19 17:53:05 +0700

    move and upload file with rename file name prevent douplicate

--- moveUpload.php
+++ moveUpload.php
@@ -6,8 +6,7 @@ include "header.php";
         <label for="">Student Name</label>
         <input type="text" name="txt-name" id="">
         <label for="">Upload Image</label>
-        <input type="file" name="txt-file" id="">
-
+        <input type="file" name="txt-file">
         <input type="submit" value="Post" name="txt-post">
     </form>
 </body>
--- my-action.php
+++ my-action.php
@@ -1,4 +1,9 @@
 <h1>
+    <!-- 
+    more info about the file upload at w3schools
+    https://www.w3schools.com/php/php_file_upload.asp
+     -->
+    
     <?php
     if (isset($_POST["txt-name"])) {
         $myName = $_POST["txt-name"];
@@ -7,9 +12,18 @@
         echo $file["size"];
         echo $file["type"];
         echo $file["tmp_name"];
+
+        $imgName = $file["name"];
+        echo $imgName;
+        echo "<br>";
+        $ext=pathinfo($imgName, PATHINFO_EXTENSION);
+        echo $ext;
+        echo "<br>";
+        $newName=time();
+        echo $newName;
         $tmp = $file["tmp_name"];
         // move the file to the folder
-        move_uploaded_file($tmp, "img/" . $file["name"]);
+        move_uploaded_file($tmp, "img/" . $newName .".". $ext);
     }else{
         echo "No action";
     }
