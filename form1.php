85fe420bc3fe8c2419215dab0d66bbd4f94fa93e
ByKimhy <kimhyby99@gmail.com>
2026-07-20 17:34:47 +0700

    create form for testing

---
+++ form1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Form1</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">ID</label><br>
+        <input type="text" name="txt-id" id=""><br>
+        <label for="">Name</label><br>
+        <input type="text" name="txt-name" id=""><br>
+        <label for="">Price</label><br>
+        <input type="text" name="txt-price" id=""><br>
+        <input type="submit" value="Post">
+    </form>
+</body>
+</html>
